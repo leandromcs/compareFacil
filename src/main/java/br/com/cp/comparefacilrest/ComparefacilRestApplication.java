998bbdc2192e577cfa808f3b0bdf4a4a9af1321d
Leandro Mendes <leandromcs96@gmail.com>
2018-10-03 13:58:47 -0300

    Arquitetura inicial do Projeto

---
+++ src/main/java/br/com/cp/comparefacilrest/ComparefacilRestApplication.java
@@ -0,0 +1,12 @@
+package br.com.cp.comparefacilrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComparefacilRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComparefacilRestApplication.class, args);
+	}
+}
