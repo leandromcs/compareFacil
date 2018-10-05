78ce1740c4bef6e09fa1e90fbb1055f990b1aba5
pedro-ramalho <pedro.ramalho@basis.com.br>
2018-10-04 21:10:28 -0300

    Alterações

--- src/main/java/br/com/cp/comparefacilrest/ComparefacilRestApplication.java
+++ src/main/java/br/com/cp/comparefacilrest/ComparefacilRestApplication.java
@@ -1,12 +1,19 @@
 package br.com.cp.comparefacilrest;
 
+import controller.UserController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
+
+@ComponentScan(basePackageClasses = { UserController.class })
 @SpringBootApplication
+
+
 public class ComparefacilRestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ComparefacilRestApplication.class, args);
+				SpringApplication.run(ComparefacilRestApplication.class, args);
 	}
 }
--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -3,20 +3,25 @@ package controller;
 import javax.ws.rs.QueryParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import model.User;
 import service.UserService;
 
-@RestController
+
+@RestController()
 public class UserController {
 	
-	@Autowired
-	private UserService service;
 
-    @GetMapping("/user")
-    public User greeting(@QueryParam(value = "nome") String nome) {
-        return service.consultarUser(nome);
+
+    @RequestMapping(value="/home",method = RequestMethod.GET)
+    public String greeting() {
+        return  "bateu aqui";
     }
 }
