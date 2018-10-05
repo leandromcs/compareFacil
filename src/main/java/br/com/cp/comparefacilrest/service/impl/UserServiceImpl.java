ac8b092852a055e8022ee21e6e8af42e5c2ca5e5
pedro-ramalho <phgr123@gmail.com>
2018-10-05 09:06:50 -0300

    Mundança na estrutura do projeto & Função de criação de Usuário

---
+++ src/main/java/br/com/cp/comparefacilrest/service/impl/UserServiceImpl.java
@@ -0,0 +1,42 @@
+package br.com.cp.comparefacilrest.service.impl;
+import br.com.cp.comparefacilrest.model.User;
+import br.com.cp.comparefacilrest.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+/**
+ * Service Implementation for managing .
+ */
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+
+    private final Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User save(User user) {
+        log.debug("Request to save UserServiceImpl : {}", user);
+        return userRepository.save(user);
+    }
+
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<User> findAll() {
+        log.debug("Request to get all Usuario");
+
+        return userRepository.findAll();
+    }
+
+  
+}
